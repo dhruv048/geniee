48d082cd384dbdcbe8d8e97da0dd6d2f4ecd9689
gyanju <gyan012shah@gmail.com>
2019-06-16 17:26:42 +0545

    local properties file added

--- SApp/app/config/settings.js
+++ SApp/app/config/settings.js
@@ -1,13 +1,15 @@
 // If you're running on a device or in the Android simulator be sure to change
 //let METEOR_URL = 'ws://192.168.1.228:3000/websocket';
-let METEOR_URL = 'ws://localhost:3000/websocket';
+let METEOR_URL = 'ws://192.168.1.228:3000/websocket';
+let API_URL = 'ws://192.168.1.228:3000/api/';
 let GOOGLE_MAP_API_KEY = 'AIzaSyCMar6aw6cYsJXNv8mHLIEaT9TCYGNefXU';
 if (process.env.NODE_ENV === 'production') {
-    METEOR_URL = 'ws://139.59.81.51/websocket'; // your production server url
+    METEOR_URL = 'ws://192.168.1.228/websocket'; // your production server url
 }
 export const settings = {
     env: process.env.NODE_ENV,
     METEOR_URL,
+    API_URL,
     GOOGLE_MAP_API_KEY,
 };
 export const userType = {
