ddd1f655075cfb88e6cdf81c524f95cd5eced030
Omkar <acanjana55@gmail.com>
2021-07-07 11:49:09 +0545

    Replace galio framework with RN paper in Contact Us Screen.

--- Geniee/app/screens/ContactUs.js
+++ Geniee/app/screens/ContactUs.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     Dimensions,
     View,
     BackHandler,
     Alert,
-    TextInput,
     StatusBar,
     TouchableOpacity,
     KeyboardAvoidingView,
@@ -13,22 +12,199 @@ import {
     Keyboard,
 } from 'react-native';
 import Meteor from '../react-native-meteor';
-import {colors} from '../config/styles';
-import {Header, Left, Body, Text, Container, Button, Textarea, Title, Icon} from 'native-base';
+import { colors } from '../config/styles';
+import { Header, Left, Body, Text, Container, Content, Button, Textarea, Title, Icon } from 'native-base';
 import CogMenu from "../components/CogMenu";
-import {Navigation} from "react-native-navigation/lib/dist/index";
-import {backToRoot, goToRoute, navigateToRoutefromSideMenu} from "../Navigation";
+import { Navigation } from "react-native-navigation/lib/dist/index";
+import { backToRoot, goToRoute, navigateToRoutefromSideMenu } from "../Navigation";
 //import Icon from 'react-native-vector-icons/Feather'
-import {GalioProvider, Input, Button as GButton} from 'galio-framework';
-import {customGalioTheme} from '../config/themes';
+// import { GalioProvider, Input, Button as GButton } from 'galio-framework';
+import { customGalioTheme } from '../config/themes';
+import { PaperProvider, TextInput,Button as GButton  } from 'react-native-paper';
 
 const window = Dimensions.get('window');
 
+class ContactUs extends Component {
+    constructor(props) {
+        super(props);
+
+        this.mounted = false;
+        this.state = {
+            name: '',
+            email: '',
+            contact: '',
+            message: '',
+            error: null,
+            loading: false,
+        };
+    }
+
+
+    componentDidMount() {
+
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton.bind(this));
+    }
+
+    handleBackButton() {
+        console.log('handlebackpress')
+        // this.props.navigation.navigate('Dashboard');
+        this.props.navigation.navigate('Home');
+        return true;
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton.bind(this));
+    }
+
+    handleError = (error) => {
+        console.log('handleError')
+        this.setState({ error });
+    }
+
+    validInput = () => {
+        const { name, email, message, contact } = this.state;
+        let valid = true;
+
+        if (!contact.length || !message || !name || !email) {
+            this.handleError('Please Enter all the information.');
+            valid = false;
+        }
+        else {
+            this.handleError('');
+            valid = true;
+        }
+
+        if (valid) {
+            this.handleError(null);
+        }
+        return valid;
+    }
+
+
+    handleContactUS = () => {
+        console.log('handleContactUS')
+        const { name, email, message, contact } = this.state;
+        if (this.validInput()) {
+            let contactInfo = {
+                name: name,
+                phone: contact,
+                email: email,
+                message: message
+            };
+            this.setState({ loading: true });
+            Meteor.call('addContactUsMessage', contactInfo, (err, result) => {
+                this.setState({ loading: false });
+                if (err) {
+                    this.handleError(err.reason);
+                } else {
+                    Alert.alert("DONE", "We will Contact you shortly!!");
+                    this.props.navigation.navigate('Dashboard');
+                }
+            });
+        }
+    }
+
+    render() {
+
+        return (
+            <TouchableWithoutFeedback
+            onPress={Keyboard.dismiss}
+            accessible={false}>
+            <Container style={styles.container}>
+               
+                <Header androidStatusBarColor={colors.statusBar} style={{ backgroundColor: colors.appLayout }}>
+                    <Left>
+                        <Button transparent onPress={() => { this.props.navigation.goBack() }}>
+                            <Icon style={{ color: '#ffffff' }} name="arrow-back" />
+                        </Button>
+                        {/* <CogMenu componentId={this.props.componentId} /> */}
+                    </Left>
+                    <Body>
+                        <Title style={styles.screenHeader}>Contact Us</Title>
+                    </Body>
+                </Header>
+                <View style={{marginVertical : 20}}>
+                    <KeyboardAvoidingView style={{ padding: 20 }}>
+                        {/*<View style={styles.header}>*/}
+                        <Text style={styles.subHeaderText}>
+                            Want to get in touch with us? Fill out the form below to send us a message and we
+                            will try
+                            to get back to you within 24 hours!
+                        </Text>
+                        {/*</View>*/}
+
+                        <View style={{marginVertical : 15}}>
+                            <TextInput
+                                mode="outlined"
+                                color={customGalioTheme.COLORS.INPUT_TEXT}
+                                placeholder='Full name'
+                                value={this.state.name}
+                                onChangeText={(name) => this.setState({ name })}
+                                style={{marginVertical :10}} />
+
+                            <TextInput
+                                mode="outlined"
+                                color={customGalioTheme.COLORS.INPUT_TEXT}
+                                placeholder='Email address'
+                                keyboardType='email-address'
+                                value={this.state.email}
+                                onChangeText={(email) => this.setState({ email })}
+                                textContentType={'emailAddress'}
+                                style={{marginVertical :10}}
+                            />
+
+                            <TextInput
+                                mode="outlined"
+                                color={customGalioTheme.COLORS.INPUT_TEXT}
+                                placeholder='Phone No.'
+                                keyboardType='phone-pad'
+                                value={this.state.contact}
+                                onChangeText={(contact) => this.setState({ contact })}
+                                style={{marginVertical :10}}
+                            />
+
+                            <TextInput
+                                mode="outlined"
+                                multiline={true}
+                                rowSpan={4}
+                                placeholder="Description (*)"
+                                // style={{height:100}}
+                                placeholderTextColor={customGalioTheme.COLORS.PLACEHOLDER}
+                                selectionColor='#ffffff'
+                                underlineColorAndroid='rgba(0,0,0,0)'
+                                //onSubmitEditing={() => this.contactNumber.focus()}
+                                value={this.state.message}
+                                onChangeText={(message) => this.setState({ message })}
+                                style={{marginVertical :10}}
+                            />
+                        </View>
+                        <View style={styles.error}>
+                            <Text style={styles.errorText}>{this.state.error}</Text>
+                        </View>
+                        <View>
+                            <GButton
+                            mode="contained"
+                                onPress={() => {
+                                    this.handleContactUS();
+                                }}
+                                style={{ width: '100%', height:50 }}
+                                loading={this.state.loading}
+                                disabled={this.state.loading}>
+                                Send
+                            </GButton>
+                        </View>
+                    </KeyboardAvoidingView>
+                </View>
+            </Container >
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+export default (ContactUs);
+
 const styles = StyleSheet.create({
     container: {
-        //flex: 1,
-        //justifyContent: 'center',
-        //alignItems: 'center',
         width: '100%',
         backgroundColor: colors.appBackground,
         fontFamily: `Source Sans Pro`,
@@ -80,35 +256,12 @@ const styles = StyleSheet.create({
         color: colors.whiteText,
         textAlign: 'center'
     },
-    //buttons: {
-    //flexDirection: 'row',
-    //justifyContent: 'center',
-    // marginTop:10
-    //},
-    //error: {
-    //flexDirection: 'row',
-    //height: 28,
-    //justifyContent: 'center',
-    //width: '100%',
-    //alignItems: 'center',
-    //},
+
     errorText: {
         color: colors.danger,
         fontSize: 14,
         marginBottom: 14,
     },
-    //header: {
-    //marginBottom: 25,
-    //alignItems: 'center',
-    //   backgroundColor:'#80ccff',
-    //padding:5
-    //},
-    //headerText: {
-    //fontSize: 30,
-    //color: colors.headerText,
-    //fontWeight: '600',
-    //fontStyle: 'italic',
-    //},
     subHeaderText: {
         fontSize: 20,
         // color: colors.headerText,
@@ -123,206 +276,3 @@ const styles = StyleSheet.create({
         marginHorizontal: 20
     },
 });
-
-class ContactUs extends Component {
-    constructor(props) {
-        super(props);
-
-        this.mounted = false;
-        this.state = {
-            name: '',
-            email: '',
-            contact: '',
-            message: '',
-            error: null,
-            loading: false,
-        };
-    }
-
-
-    componentDidMount() {
-        
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton.bind(this));
-    }
-
-    handleBackButton() {
-        console.log('handlebackpress')
-        // this.props.navigation.navigate('Dashboard');
-        this.props.navigation.navigate('Home');
-        return true;
-    }
-
-    componentWillUnmount() {
-        BackHandler.removeEventListener('hardwareBackPress',this.handleBackButton.bind(this));
-    }
-
-    handleError = (error) => {
-        console.log('handleError')
-        this.setState({error});
-    }
-
-    validInput = () => {
-        const {name, email, message, contact} = this.state;
-        let valid = true;
-
-        if (!contact.length || !message || !name || !email) {
-            this.handleError('Please Enter all the information.');
-            valid = false;
-        }
-        else {
-            this.handleError('');
-            valid = true;
-        }
-
-        if (valid) {
-            this.handleError(null);
-        }
-        return valid;
-    }
-
-
-    handleContactUS = () => {
-        console.log('handleContactUS')
-        const {name, email, message, contact} = this.state;
-        if (this.validInput()) {
-            let contactInfo = {
-                name: name,
-                phone: contact,
-                email: email,
-                message: message
-            };
-            this.setState({loading: true});
-            Meteor.call('addContactUsMessage', contactInfo, (err, result) => {
-                this.setState({loading: false});
-                if (err) {
-                    this.handleError(err.reason);
-                } else {
-                    Alert.alert("DONE", "We will Contact you shortly!!");
-                    this.props.navigation.navigate( 'Dashboard');
-                }
-            });
-        }
-    }
-
-    render() {
-
-        return (
-            <GalioProvider theme={customGalioTheme}>
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-
-                    <Container style={styles.container}>
-                        <Header androidStatusBarColor={colors.statusBar} style={{backgroundColor: colors.appLayout}}>
-                            <Left>
-                                {/*<Button transparent onPress={()=>{this.props.navigation.goBack()}}>*/}
-                                {/*<Icon style={{color: '#ffffff'}} name="arrow-back" />*/}
-                                {/*</Button>*/}
-                                <CogMenu componentId={this.props.componentId}/>
-                            </Left>
-                            <Body>
-                            <Title style={styles.screenHeader}>Contact Us</Title>
-                            </Body>
-                        </Header>
-                        <View style={styles.contentContainer}>
-                            <KeyboardAvoidingView style={{padding: 20}}>
-                                {/*<View style={styles.header}>*/}
-                                <Text style={styles.subHeaderText}>
-                                    Want to get in touch with us? Fill out the form below to send us a message and we
-                                    will try
-                                    to get back to you within 24 hours!
-                                </Text>
-                                {/*</View>*/}
-
-                                <View><Input
-                                    color={customGalioTheme.COLORS.INPUT_TEXT}
-                                    placeholder='Full name'
-                                    onChangeText={(name) => this.setState({name})}/>
-
-                                    <Input
-                                        color={customGalioTheme.COLORS.INPUT_TEXT}
-                                        placeholder='Email address'
-                                        keyboardType='email-address'
-                                        onChangeText={(email) => this.setState({email})}
-                                        textContentType={'emailAddress'}
-                                    />
-
-                                    <Input
-                                        color={customGalioTheme.COLORS.INPUT_TEXT}
-                                        placeholder='Phone No.'
-                                        keyboardType='phone-pad'
-                                        onChangeText={(contact) => this.setState({contact})}
-                                    />
-
-                                    <Textarea rowSpan={4} placeholder="Description (*)"
-                                              style={styles.inputTextarea}
-                                              placeholderTextColor={customGalioTheme.COLORS.PLACEHOLDER}
-                                              selectionColor='#ffffff'
-                                              underlineColorAndroid='rgba(0,0,0,0)'
-                                        //onSubmitEditing={() => this.contactNumber.focus()}
-                                              onChangeText={(message) => this.setState({message})}
-                                    /></View>
-
-                                {/*<Item floatingLabel>
-                            <Label>Full name</Label>
-                        <Input
-                            onChangeText={(name) => this.setState({name})}
-                        />
-                        </Item>
-                        <Item floatingLabel>
-                            <Label>Email address</Label>
-                            <Input
-                            onChangeText={(email) => this.setState({email})} />
-                        </Item>
-                        <Item floatingLabel>
-                            <Label>Phone No.</Label>
-                            <Input
-                            onChangeText={(contact) => this.setState({contact})}
-                        />
-                        </Item>*/}
-                                {/*<Item floatingLabel>*/}
-                                {/*<Label>Message</Label>*/}
-                                {/*<Textarea underline placeholder="Message"
-                            onChangeText={(message) => this.setState({message})}
-                            rowSpan={5}
-                    />*/}
-                                {/*</Item>*/}
-                                <View style={styles.error}>
-                                    <Text style={styles.errorText}>{this.state.error}</Text>
-                                </View>
-                                <View>
-                                    <GButton
-                                        onPress={() => {
-                                            this.handleContactUS();
-                                        }}
-                                        style={{width: '100%', marginVertical: 20}}
-                                        loading={this.state.loading}
-                                        disabled={this.state.loading}>
-                                        Send
-                                    </GButton>
-                                    {/*   <TouchableOpacity  style={styles.button}  onPress={()=>this.handleContactUS()}>
-                                    <Text style={styles.buttonText}>SEND</Text>
-                                </TouchableOpacity>*/}
-                                </View>
-
-                                {/*<View style={styles.buttons}>
-                        <Button block  onPress={this.handleContactUS}>
-                            <Text>SEND</Text>
-                </Button>*/}
-                                {/*<Button text="Cancel" onPress={() => {*/}
-                                {/*this.setState({*/}
-                                {/*error: null*/}
-                                {/*}), this.props.navigation.navigate('Home')*/}
-                                {/*}}/>*/}
-                                {/*</View>*/}
-                                {/*<KeyboardSpacer/>*/}
-                            </KeyboardAvoidingView>
-
-                        </View>
-
-                    </Container>
-                </TouchableWithoutFeedback>
-            </GalioProvider>
-        );
-    }
-}
-
-export default (ContactUs);
